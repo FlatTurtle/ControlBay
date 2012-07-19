8777e2f5082102338d8283af557a60929ef8f0d9
Nik Torfs <ntorfs@gmail.com>
2012-07-19 19:23:56 +0200

    removed https stuff, this is handled with nginx

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -4,26 +4,6 @@ class MY_Controller extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->output->set_content_type('application/json');
-        $this->load->helper('url');
-        if(defined('ENVIRONMENT')){
-            switch (ENVIRONMENT)
-            {
-                case 'development':
-                    break;
-                default :
-                    if (array_key_exists('HTTPS', $_SERVER)) {
-                        if ($_SERVER['HTTPS'] != 'on') {
-                            header('Location:'.base_url(uri_string()));
-                            exit;
-                        }
-                    } //check on https connection, could also check $_SERVER['HTTPS'] but isn't provided on every server
-					else if ($_SERVER['SERVER_PORT'] != 443) {
-                        header('Location:'.base_url(uri_string()));
-                        exit;
-                    }
-                    break;
-            }
-        }
     }
 
     /**
